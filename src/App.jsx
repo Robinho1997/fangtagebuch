1ea3a2110ad604793f6266430015dd16aed16528
Robinho1997 <robin.kempen@gmx.de>
2023-05-25 16:29:13 +0200

    App & Main update

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,88 @@
-import React from 'react'
-import { initializeApp } from "firebase/app"
-import { getDatabase, ref } from "firebase/database";
+import React, { useEffect, useState } from 'react'
+import Navbar from './components/Navbar';
 import Form from './components/Form';
 import Fänge from './components/Fänge';
+import RegisterUser from './components/RegisterUser';
+import LoginUser from './components/LoginUser';
+import { authentication } from './Firebase';
 
 function App() {
+  const [displayRegisterForm, setDisplayRegisterForm] = useState(false);
+  const [displayLoginForm, setDisplayLoginForm] = useState(false);
 
-  const firebaseConfig = {
-    databaseURL: "https://fangtagebuch-6db8d-default-rtdb.europe-west1.firebasedatabase.app/",
-    storageBucket: 'gs://fangtagebuch-6db8d.appspot.com',
-  };
-
-  const app = initializeApp(firebaseConfig);
-  const database = getDatabase(app);
-
-  const fängeInDB = ref(database,"fänge")
-
-
+  const [user, setUser] = useState(null);
+  // Check if user ist eingeloggt
+  useEffect(() => {
+    const unsubscribe = authentication.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+        console.log(user)
+      } else {
+        setUser(null);
+        console.log(user)
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   return (
     <div>
-      <h1 style={{textAlign:"center"}}>Fangtagebuch</h1>
-      <Form fängeInDB={fängeInDB}/>
-      <Fänge fängeInDB={fängeInDB} database={database}/>
+
+      <Navbar setDisplayRegisterForm={setDisplayRegisterForm} setDisplayLoginForm={setDisplayLoginForm} />
+
+      {user ? (
+        <div>
+          <h1 style={{ textAlign: "center", marginTop: "30px" }}>Fangtagebuch</h1>
+          <Form user={user} />
+        </div>
+      ) : (
+        <main className='start-page-content'>
+          <div className='welcome-div'>
+            <div className='shadow'>
+              <h1 style={{ textAlign: "center", marginTop: "0" }}>Fangtagebuch</h1>
+              <p>Erstelle dein kostenloses Fang-Tagebuch und halte deine Angelerfolge fest.</p>
+              <button onClick={() => { setDisplayRegisterForm(prev => !prev) }}>Jetzt kostenlos registrieren!</button>
+            </div>
+          </div>
+
+          <section className="features-section">
+            <h2>Warum Angler's Catch Diary nutzen?</h2>
+
+            <div className="feature">
+
+              <h3>Fangprotokolle erstellen</h3>
+              <p>Halte deine Fänge in detaillierten Protokollen fest und behalte den Überblick über deine Angelerfolge.</p>
+            </div>
+            <div className="feature">
+
+              <h3>Fotogalerie</h3>
+              <p>Lade Fotos deiner Fänge hoch und vergesse damit nie die dazugehörigen Informationen zu deinen Lieblingsfängen.</p>
+            </div>
+
+          </section>
+
+          <section className="signup-section">
+            <h2>Jetzt kostenlos registrieren</h2>
+            <p>Erstelle noch heute dein Angler's Catch Diary und verpasse keine Details mehr von deinen Angelerlebnissen.</p>
+            <button onClick={() => { setDisplayRegisterForm(prev => !prev) }}>Kostenlosen Account erstellen</button>
+          </section>
+        </main>
+      )}
+
+      <Fänge />
+      <RegisterUser displayRegisterForm={displayRegisterForm} setDisplayRegisterForm={setDisplayRegisterForm} hasRegistered={user} />
+      <LoginUser displayLoginForm={displayLoginForm} setDisplayLoginForm={setDisplayLoginForm} isLoggedIn={user} />
+      {/* 
+      <footer>
+        <p>&copy; 2023 Fangtagebuch. Alle Rechte vorbehalten.</p>
+      </footer>
+      */ }
     </div>
 
   )
 }
 
 export default App
+
