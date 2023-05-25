0f73443b6889b329c68667f22f5d55c616830993
Robinho1997 <robin.kempen@gmx.de>
2023-05-25 16:26:04 +0200

    Login User Component

---
+++ src/components/LoginUser.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { authentication } from "../Firebase";
+
+function LoginUser(props) {
+
+    const [inputOfEmail, setInputOfEmail] = useState("");
+    const [inputOfPassword, setInputOfPassword] = useState("");
+
+    const [loginMessage, setLoginMessage] = useState("Logged in")
+
+    function handleSignIn(e) {
+        e.preventDefault();
+
+        signInWithEmailAndPassword(authentication, inputOfEmail, inputOfPassword)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+
+
+                console.log("Logged in")
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log("errorCode:" + errorCode + "errorMessage:" + errorMessage)
+                setLoginMessage(errorMessage)
+            });
+    };
+
+    return (
+        <div>
+            {
+                props.displayLoginForm &&
+                <div className="authentification-form">
+                    <button onClick={() => props.setDisplayLoginForm(false)} className="close-btn">
+                        <span className="material-symbols-outlined">
+                            cancel
+                        </span>
+                    </button>
+                    {props.isLoggedIn ? <p>{loginMessage}</p> :
+                        <div>
+                            <h2>Login <span className="material-symbols-outlined">
+                                login
+                            </span></h2>
+                            <input value={inputOfEmail} onChange={(e) => setInputOfEmail(e.target.value)} type="email" placeholder="Email" />
+                            <input value={inputOfPassword} onChange={(e) => setInputOfPassword(e.target.value)} type="password" placeholder="Passwort" />
+                            <button className="form-btn" onClick={handleSignIn}>
+                                Login
+                            </button>
+                        </div>
+                    }
+
+                </div>
+
+            }
+        </div>
+    );
+}
+
+export default LoginUser;
