7e6bba62516bb5ccd6dbfe1264e030545e3c39dc
Robinho1997 <robin.kempen@gmx.de>
2023-05-31 01:32:47 +0200

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import AddFangForm from "./components/AddFangForm";
-import Fänge from "./components/Fänge";
+import DisplayLoggedInUserData from "./components/DisplayLoggedInUserData";
 import RegisterUser from "./components/RegisterUser";
 import LoginUser from "./components/LoginUser";
 import Homepage from "./components/Homepage";
 import Footer from "./components/Footer";
 import { authentication } from "./Firebase";
-
-
+import { onAuthStateChanged } from "firebase/auth";
 function App() {
   const [displayRegisterForm, setDisplayRegisterForm] = useState(false);
   const [displayLoginForm, setDisplayLoginForm] = useState(false);
@@ -16,48 +15,53 @@ function App() {
 
   // Check if user ist eingeloggt
   useEffect(() => {
-    const unsubscribe = authentication.onAuthStateChanged((user) => {
+    onAuthStateChanged(authentication, (user) => {
       if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+
         setUser(user);
-        console.log(user);
+        // ...
       } else {
+        // User is signed out
+        // ...
         setUser(null);
-        console.log(user);
       }
     });
-    return () => {
-      unsubscribe();
-    };
   }, []);
 
   return (
-    <div>
-      <Navbar
-        setDisplayRegisterForm={setDisplayRegisterForm}
-        setDisplayLoginForm={setDisplayLoginForm}
-      />
-      {user ? (
-        <div>
-          <h1 style={{ textAlign: "center", marginTop: "30px" }}>
-            Fangtagebuch
-          </h1>
+    <div className="app">
+      <div className="content">
+        <Navbar
+          setDisplayRegisterForm={setDisplayRegisterForm}
+          setDisplayLoginForm={setDisplayLoginForm}
+          user={user}
+        />
+        {user ? (
           <AddFangForm user={user} />
-        </div>
-      ) : (
-        <Homepage />
-      )}
-      <Fänge />
-      <RegisterUser
-        displayRegisterForm={displayRegisterForm}
-        setDisplayRegisterForm={setDisplayRegisterForm}
-        hasRegistered={user}
-      />
-      <LoginUser
-        displayLoginForm={displayLoginForm}
-        setDisplayLoginForm={setDisplayLoginForm}
-        isLoggedIn={user}
-      />
-      <Footer />
+        ) : (
+          <Homepage
+            displayRegisterForm={displayRegisterForm}
+            setDisplayRegisterForm={setDisplayRegisterForm}
+          />
+        )}
+        {user && <DisplayLoggedInUserData user={user} />}
+
+        <RegisterUser
+          displayRegisterForm={displayRegisterForm}
+          setDisplayRegisterForm={setDisplayRegisterForm}
+          hasRegistered={user}
+        />
+        <LoginUser
+          displayLoginForm={displayLoginForm}
+          setDisplayLoginForm={setDisplayLoginForm}
+          isLoggedIn={user}
+        />
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
   );
 }
