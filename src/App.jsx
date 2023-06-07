132f531c531eabfd78f666d5b8f7d407e0a38245
Robinho1997 <robin.kempen@gmx.de>
2023-06-07 04:36:26 +0200

    Added Routes % Context Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import AddFangForm from "./components/AddFangForm";
 import DisplayLoggedInUserData from "./components/DisplayLoggedInUserData";
@@ -6,59 +6,55 @@ import RegisterUser from "./components/RegisterUser";
 import LoginUser from "./components/LoginUser";
 import Homepage from "./components/Homepage";
 import Footer from "./components/Footer";
-import { authentication } from "./Firebase";
-import { onAuthStateChanged } from "firebase/auth";
+import Lexikon from "./components/Lexikon";
+import LexikonEintrag from "./components/LexikonEintrag";
+import { Context } from "./Context";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
-  const [displayRegisterForm, setDisplayRegisterForm] = useState(false);
-  const [displayLoginForm, setDisplayLoginForm] = useState(false);
-  const [user, setUser] = useState(null);
-
-  // Check if user ist eingeloggt
-  useEffect(() => {
-    onAuthStateChanged(authentication, (user) => {
-      if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/auth.user
-
-        setUser(user);
-        // ...
-      } else {
-        // User is signed out
-        // ...
-        setUser(null);
-      }
-    });
-  }, []);
+  const {
+    user,
+    displayRegisterForm,
+    setDisplayRegisterForm,
+    displayLoginForm,
+    setDisplayLoginForm,
+  } = useContext(Context);
 
   return (
     <div className="app">
-      <div className="content">
-        <Navbar
-          setDisplayRegisterForm={setDisplayRegisterForm}
-          setDisplayLoginForm={setDisplayLoginForm}
-          user={user}
-        />
-        {user ? (
-          <AddFangForm user={user} />
-        ) : (
-          <Homepage
-            displayRegisterForm={displayRegisterForm}
-            setDisplayRegisterForm={setDisplayRegisterForm}
-          />
-        )}
-        {user && <DisplayLoggedInUserData user={user} />}
+      <Navbar
+        setDisplayRegisterForm={setDisplayRegisterForm}
+        setDisplayLoginForm={setDisplayLoginForm}
+        user={user}
+      />
+      {user && ( // wenn eingeloggt fangform zeigen
+        <AddFangForm user={user} />
+      )}
+      {user && <DisplayLoggedInUserData user={user} />}
+      <RegisterUser
+        displayRegisterForm={displayRegisterForm}
+        setDisplayRegisterForm={setDisplayRegisterForm}
+        hasRegistered={user}
+      />
+      <LoginUser
+        displayLoginForm={displayLoginForm}
+        setDisplayLoginForm={setDisplayLoginForm}
+        isLoggedIn={user}
+      />
 
-        <RegisterUser
-          displayRegisterForm={displayRegisterForm}
-          setDisplayRegisterForm={setDisplayRegisterForm}
-          hasRegistered={user}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Homepage
+              displayRegisterForm={displayRegisterForm}
+              setDisplayRegisterForm={setDisplayRegisterForm}
+            />
+          }
         />
-        <LoginUser
-          displayLoginForm={displayLoginForm}
-          setDisplayLoginForm={setDisplayLoginForm}
-          isLoggedIn={user}
-        />
-      </div>
+        <Route path="/lexikon" element={<Lexikon />} />
+        <Route path="lexikon/:name" element={<LexikonEintrag />} />
+      </Routes>
       <div className="footer">
         <Footer />
       </div>
