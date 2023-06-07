cc91b66759ba66029f2ce1dedd484418fc80a7cf
Robinho1997 <robin.kempen@gmx.de>
2023-06-07 04:36:02 +0200

    Added Context

---
+++ src/Context.jsx
@@ -0,0 +1,38 @@
+import React, { createContext, useEffect, useState } from "react";
+import { authentication } from "./Firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
+const Context = createContext();
+
+function ContextProvider(props) {
+  const [user, setUser] = useState(null);
+  const [displayRegisterForm, setDisplayRegisterForm] = useState(false);
+  const [displayLoginForm, setDisplayLoginForm] = useState(false);
+  // Check if user ist eingeloggt
+  useEffect(() => {
+    onAuthStateChanged(authentication, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        // User is signed out
+        setUser(null);
+      }
+    });
+  }, []);
+
+  return (
+    <Context.Provider
+      value={{
+        user,
+        displayRegisterForm,
+        setDisplayRegisterForm,
+        displayLoginForm,
+        setDisplayLoginForm,
+      }}
+    >
+      {props.children}
+    </Context.Provider>
+  );
+}
+
+export { ContextProvider, Context };
