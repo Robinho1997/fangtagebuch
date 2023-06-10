9c69ed7b88e5f95fd15ab7f7f57f22c88a012ec1
Robinho1997 <robin.kempen@gmx.de>
2023-06-10 14:09:34 +0200

    setImg

--- src/components/LexikonEintragAbschnitte/Beschreibung.jsx
+++ src/components/LexikonEintragAbschnitte/Beschreibung.jsx
@@ -9,6 +9,87 @@ function Beschreibung() {
   const databaseReference = ref(getDatabase());
   const [systematikData, setSystematikData] = useState(null);
   const [beschreibungData, setBeschreibungData] = useState(null);
+  const [img, setImg] = useState(null);
+
+  useEffect(() => {
+    switch (name) {
+      case "Aal":
+        setImg("/public/fische/aal.webp");
+        break;
+      case "Äsche":
+        setImg("/public/fische/äsche.webp");
+        break;
+      case "Aland":
+        setImg("/public/fische/aland.webp");
+        break;
+      case "Bachforelle":
+        setImg("/public/fische/bachforelle.webp");
+        break;
+      case "Barbe":
+        setImg("/public/fische/barbe.webp");
+        break;
+      case "Barsch":
+        setImg("/public/fische/barsch.webp");
+        break;
+      case "Brasse":
+        setImg("/public/fische/brassen.png");
+        break;
+      case "Döbel":
+        setImg("/public/fische/doebel.webp");
+        break;
+      case "Giebel":
+        setImg("/public/fische/giebel.png");
+        break;
+      case "Gründling":
+        setImg("/public/fische/gruendling.webp");
+        break;
+      case "Güster":
+        setImg("/public/fische/guester.png");
+        break;
+      case "Hasel":
+        setImg("/public/fische/hasel.webp");
+        break;
+      case "Hecht":
+        setImg("/public/fische/hecht.webp");
+        break;
+      case "Karausche":
+        setImg("/public/fische/karausche.png");
+        break;
+      case "Karpfen":
+        setImg("/public/fische/karpfen.webp");
+        break;
+      case "Nase":
+        setImg("/public/fische/nase.webp");
+        break;
+      case "Rapfen":
+        setImg("/public/fische/rapfen.webp");
+        break;
+      case "Regenbogenforelle":
+        setImg("/public/fische/regenbogenforelle.webp");
+        break;
+      case "Rotauge":
+        setImg("/public/fische/rotauge.webp");
+        break;
+      case "Rotfeder":
+        setImg("/public/fische/rotfeder.png");
+        break;
+      case "Schleie":
+        setImg("/public/fische/schleie.webp");
+        break;
+      case "Ukelei":
+        setImg("/public/fische/ukelei.webp");
+        break;
+      case "Wels":
+        setImg("/public/fische/wels.webp");
+        break;
+      case "Zährte":
+        setImg("/public/fische/zaehrte.webp");
+        break;
+      case "Zander":
+        setImg("/public/fische/zander.webp");
+        break;
+    }
+  }, []);
 
   useEffect(() => {
     get(child(databaseReference, `lexikon/${name.toLowerCase()}/systematik`))
@@ -54,26 +135,25 @@ function Beschreibung() {
               <td>Name:</td>
               <td>{name}</td>
             </tr>
-            <tr className="systematik-tr">
-              <td>Besonderheit:</td>
-              <td>{systematikData && systematikData.besonderheit}</td>
-            </tr>
+
             <tr className="systematik-tr">
               <td>Wissenschaftlich:</td>
               <td>{systematikData && systematikData.wissenschaftlich}</td>
             </tr>
+            <tr className="systematik-tr">
+              <td>Familie:</td>
+              <td>{systematikData && systematikData.familie}</td>
+            </tr>
+
             <tr>
-              <td style={{ padding: "10px 20px" }}>Familie:</td>
+              <td style={{ padding: "10px 20px" }}>Besonderheit:</td>
               <td style={{ padding: "10px 20px" }}>
-                {systematikData && systematikData.familie}
+                {systematikData && systematikData.besonderheit}
               </td>
             </tr>
           </tbody>
         </table>
-        <img
-          className="fisch-img"
-          src="https://www.anglermap.de/images/fischlexikon/fischgrafik/aal1-500c.png?ezimgfmt=rs:500x250/rscb1/ngcb1/notWebP"
-        />
+        <img className="fisch-img" src={img} />
         {beschreibungData &&
           beschreibungData.map((p) => <p key={nanoid()}>{p}</p>)}
       </div>
